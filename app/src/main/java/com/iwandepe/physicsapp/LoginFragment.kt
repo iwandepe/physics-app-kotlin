e90eddcb8637c96a2ffed6677d00754be09754b3
iwandepe <iwan.dprakoso@gmail.com>
2021-02-25 08:35:42 +0700

    add login validation

--- app/src/main/java/com/iwandepe/physicsapp/LoginFragment.kt
+++ app/src/main/java/com/iwandepe/physicsapp/LoginFragment.kt
@@ -4,11 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.iwandepe.physicsapp.databinding.FragmentLoginBinding
 
-class LoginFragment : Fragment() {
+class LoginFragment : Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater, R.layout.fragment_login, container, false)
+        var editTextEmail: EditText = binding.emailInput
+        var editTextPassword: EditText = binding.passwordInput
+        binding.loginButton.setOnClickListener { v: View ->
+            val email:String = editTextEmail.text.toString()
+            val password:String = editTextPassword.text.toString()
+
+            // Validate input email and password
+            if (email.isEmpty()){
+                editTextEmail.error = "Email cannot empty"
+            }
+            if (password.isEmpty()){
+                editTextPassword.error = "Password cannot empty"
+            }
+            else if (password.length < 6) {
+                editTextPassword.error = "Password cannot less than 6 character"
+            }
+
+            // Check if the email and password are correct
+            if (email.equals("iwan@gmail.com") && password.equals("iwandwiprakoso")) {
+                v.findNavController().navigate(R.id.action_loginFragment_to_menuFragment)
+            }
+            else {
+                Toast.makeText(requireActivity(), "Email or password wrong", Toast.LENGTH_LONG).show()
+            }
+        }
+        return binding.root
     }
 }
--- app/src/main/java/com/iwandepe/physicsapp/MainActivity.kt
+++ app/src/main/java/com/iwandepe/physicsapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.iwandepe.physicsapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import com.iwandepe.physicsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+//        val navController = this.findNavController(R.id.nav_host_fragment)
     }
 }
